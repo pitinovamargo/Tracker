9792ab16552051c72a203e7d77ff58741d8be34b
Margarita <pitinovamargo@gmail.com>
2023-07-05 22:11:15 +0200

    Сверстан LaunchScreen

---
+++ Tracker/Helper/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  Tracker
+//
+//  Created by Margarita Pitinova on 05.07.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+}
