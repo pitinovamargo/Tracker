6f39877045e21e9b3dd7d53640d41e62aa650b66
Margarita <pitinovamargo@gmail.com>
2023-08-18 19:33:02 +0200

    Удалена ненужная инициализация

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -10,7 +10,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?    
-    var trackerCategoryStore = TrackerCategoryStore()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
